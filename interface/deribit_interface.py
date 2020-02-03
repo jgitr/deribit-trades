281700544577f43b32af4f8fa83a7bf15a857de4
jgitr <julian.winkel@hu-berlin.de>
2020-02-03 19:36:23 +0100

    init - successfully connecting to deribit on a private account and created a bunch of methods according to their api. scraping orderbooks for a customized set of instruments and extracting arbitrary elements from  the corresponding json response. appending in [timestamp, dict] csv style.

---
+++ interface/deribit_interface.py
@@ -0,0 +1,257 @@
+from websocket import create_connection
+import websocket
+from datetime import datetime
+import json
+import threading
+
+def thread_decor(my_func):
+	def wrapper(*args, **kwargs):
+		my_thread = threading.Thread(target=my_func, args=args, kwargs=kwargs)
+		my_thread.start()
+	return wrapper
+
+
+class Deribit:
+	def __init__(self, test, only_public=False, client_ID=False, client_secret=False):
+		if client_ID or client_secret: only_public = False
+		if only_public:
+			self.logwritter(msg='WARNING! Only public methods available!')
+			return
+		self.WSS_url = 'wss://www.test.deribit.com/ws/api/v2' if test else 'wss://www.deribit.com/ws/api/v2'
+		self._auth(client_ID, client_secret, self.WSS_url)
+
+
+
+	def logwritter(self, msg, filename='log.log'):
+		out = datetime.now().strftime("\n[%Y%m%d,%H:%M:%S] ")+str(msg)
+		print(out)
+		open(filename, 'a').write(out)
+
+
+
+	def _auth(self, client_ID, client_secret, WSS_url):
+		try:
+			self._WSS = create_connection(WSS_url)
+			msg = {"jsonrpc" : "2.0",
+				  "id" : 9929,
+				  "method" : "public/auth",
+				  "params" : {
+					"grant_type" : "client_credentials",
+					"client_id" : client_ID,
+					"client_secret" : client_secret
+				  }
+				}
+			self.logwritter('Auth OK\n############')
+			print('identified')
+			return self._sender(msg)
+		except Exception as er:
+			self.logwritter('auth error:'+str(er))
+
+
+
+	def _sender(self, msg):
+		try:
+			self.logwritter(msg['method'])
+			self._WSS.send(json.dumps(msg))
+			out = json.loads(self._WSS.recv())
+			return out['result']
+		except Exception as er:
+			self.logwritter(str(out))
+			self.logwritter('_sender error: '+str(er))
+
+
+
+	def make_order(self,
+				side,
+				instrument_name,
+				amount,
+				type_ord='limit',
+				label=None,
+				price=None,
+				time_in_force='good_til_cancelled',
+				max_show=False,
+				post_only=True,
+				reduce_only=False,
+				trigger=None):
+		if not side=='buy' and not side=='sell':
+			self.logwritter('ERROR: incorect param "side" for make_order')
+			return
+		msg ={
+			  "jsonrpc": "2.0",
+			  "id" : 5275,
+			  "method": "private/"+str(side),
+			  "params": {
+				"instrument_name" : instrument_name,
+				"amount": amount,
+				"type": type_ord,
+				"label": label,
+				"price": price,
+				"time_in_force": time_in_force,
+			   	"post_only": post_only,
+			   	"reduce_only": reduce_only,
+			   	"trigger":trigger}
+			}
+		if max_show: msg['params']['max_show'] = max_show
+		return self._sender(msg)
+
+
+
+	def edit_order(self,
+				order_id,
+				amount,
+				price,
+				post_only=True,
+				stop_price=None):
+		msg ={
+			  "jsonrpc": "2.0",
+			  "id" : 5275,
+			  "method": "private/edit",
+			  "params": {
+			  "order_id": order_id,
+				"amount": amount,
+				"price": price,
+			   	#"post_only": post_only,
+			   	#"stop_price": stop_price
+			   	}
+			}
+		return self._sender(msg)
+
+
+
+	def cancel_order(self, order_id):
+		msg ={
+			  "jsonrpc": "2.0",
+			  "id" : 5275,
+			  "method": "private/cancel",
+			  "params": {
+				"order_id": order_id}
+			}
+		return self._sender(msg)
+
+
+
+	def get_order_state(self, order_id):
+		msg ={
+			  "jsonrpc": "2.0",
+			  "id" : 5275,
+			  "method": "private/get_order_state",
+			  "params": {
+				"order_id": order_id}
+			}
+		return self._sender(msg)
+
+
+
+	def get_order_book(self, instrument_name, depth=1):
+		msg ={
+		  "jsonrpc": "2.0",
+		  "id" : None,
+		  "method": "public/get_order_book",
+		  "params": {
+			"instrument_name": instrument_name,
+			"depth": depth}
+		}
+		return self._sender(msg)
+
+	
+	
+	def get_chart_data(self, instrument_name,
+					start_timestamp, end_timestamp,
+					resolution):
+		msg ={
+		  "jsonrpc": "2.0",
+		  "id" : None,
+		  "method": "public/get_tradingview_chart_data",
+		  "params": {
+			"instrument_name": instrument_name,
+			"start_timestamp": start_timestamp,
+			'end_timestamp': end_timestamp,
+			'resolution': resolution}
+		}
+		return self._sender(msg)
+
+	
+	
+	@thread_decor
+	def start_orderbook_update(self, instrument_name='BTC-PERPETUAL'): # current orderbook contain in 'Orderbook'
+		self.__first = True
+		msg = {"jsonrpc": "2.0",
+			 "method": "public/subscribe",
+			 "id": 42,
+			 "params": {
+				"channels": ["book."+str(instrument_name)+".none.1.100ms"]}
+			}
+		try:
+			def on_message(ws, message):
+				print(message)
+				if self.__first: self.__first=False; return
+				#self.logwritter('Orderbook')
+				self.Orderbook = json.loads(message)['params']['data']
+				#if func_for_quoting: func_for_quoting() # Запуск вспомогательной функции, если она есть.
+			def on_error(ws, error):
+				self.logwritter('Orderbook updater error: '+str(error))
+			def on_close(ws):
+				self.logwritter('Orderbook updater error:closed connect')
+			def on_open(ws):
+				ws.send(json.dumps(msg))
+			websocket.enableTrace(True)
+			ws = websocket.WebSocketApp(self.WSS_url,
+									  on_message = on_message,
+									  on_error = on_error,
+									  on_close = on_close)
+			ws.on_open = on_open
+			ws.run_forever()
+		except Exception as er:
+			self.logwritter('Orderbook updater error: '+str(er))
+
+	def get_user_trades_by_instrument(self, _currency):
+		msg = {
+			"jsonrpc": "2.0",
+			"method": "private/get_user_trades_by_currency",
+			"params": {
+				'currency' : _currency,
+				"start_id": "ETH-34066",
+				"count": 2
+			}
+		}
+
+	def get_transactions(self, _currency, _count):
+		msg = {
+			"jsonrpc": "2.0",
+			"method": "private/get_order_history_by_currency",
+			"params": {
+				"currency": _currency,
+				"count": _count}
+		}
+		return self._sender(msg)
+
+	def test(self):
+		msg = {
+			"jsonrpc": "2.0",
+			"method": "private/get_last_trades_by_currency",
+			"params" : {}
+		}
+		return self._sender(msg)
+
+	def get_last_trades_by_currency(self, _currency, _kind):
+		msg = {
+			"jsonrpc": "2.0",
+			"method": "private/get_last_trades_by_currency",
+			"params": {
+				"currency": _currency,
+				"kind" : _kind}
+		}
+		return self._sender(msg)
+'''
+ You can add new methods. Example:
+
+	def name_method(self, param1, param2):
+		msg ={
+		  "jsonrpc": "2.0",
+		  "method": "public/METHOD",
+		  "params": {
+			"param1": param1,
+			"param2": param2}
+		}
+		return self._sender(msg)
+'''
---
+++ main.py
@@ -0,0 +1,56 @@
+import asyncio
+import json
+import time
+import csv
+from interface import deribit_interface
+
+CLIENT_ID = '2MgTUxpK'
+CLIENT_SECRET = 'IPoES_Vpvb3-3XqJi2IAdZvX4XmooHUxeIPw4831BEM'
+
+### system value ###
+deribit = deribit_interface.Deribit(test=False,
+	client_ID=CLIENT_ID,
+ 	client_secret=CLIENT_SECRET)
+logwritter = deribit.logwritter
+
+def construct_instruments(maturitystr, strike):
+	call = 'BTC-' + maturitystr + '-' + str(strike) + '-C'
+	put = 'BTC-' + maturitystr + '-'+ str(strike) + '-P'
+	return call, put
+
+
+all_maturitystr = ['14FEB20', '28FEB20']
+all_strikes = [10000, 10250]
+all_instruments = []
+
+for maturity in all_maturitystr:
+	for k in all_strikes:
+		call, put = construct_instruments(maturity, k)
+		all_instruments.append(call)
+		all_instruments.append(put)
+print(all_instruments)
+
+#trades = deribit.get_user_trades_by_instrument('ETH')
+#trades = deribit.get_transactions('BTC', 10)
+#trades = deribit.get_last_trades_by_currency('BTC', 'option') # TO BE DONE
+#print(trades)
+
+def extract_greeks(instrument, ob):
+	return [instrument, ob['greeks']]
+
+def save_dict_to_file(dic):
+	# Todo: This must be appended for each dictionary to come
+    f = open('dict.txt','a')
+    f.write(str(dic) + '\n')
+    f.close()
+
+pausetime = 1
+while True:
+	for instrument in all_instruments:
+		ob = deribit.get_order_book(instrument)
+		if ob is not None:
+			print(instrument)
+			print(ob)
+			save_dict_to_file(extract_greeks(instrument, ob))
+		time.sleep(1)
+
