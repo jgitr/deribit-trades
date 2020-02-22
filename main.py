6448fadb76ad68f5cd55c97f6c87298658983245
lax <winjules2@gmail.com>
2020-02-22 18:19:32 +0100

    create instruments from server

--- main.py
+++ main.py
@@ -17,25 +17,22 @@ deribit = deribit_interface.Deribit(test=False,
 logwritter = deribit.logwritter
 
 def construct_instruments(maturitystr, strike):
+	# Manually create instrument
 	call = 'BTC-' + maturitystr + '-' + str(strike) + '-C'
 	put = 'BTC-' + maturitystr + '-'+ str(strike) + '-P'
 	return call, put
 
 
-all_maturitystr = ['28FEB20']
-all_strikes = [10000, 10250]
-all_instruments = []
-
-for maturity in all_maturitystr:
-	for k in all_strikes:
-		call, put = construct_instruments(maturity, k)
-		all_instruments.append(call)
-		all_instruments.append(put)
-print(all_instruments)
-
-# Retrieving trades works now // example
-#trades = deribit.get_last_trades_by_currency('BTC', 'option', 30) # TO BE DONE
-#print(trades)
+def create_instruments(_currency, _kind):
+	# Get all instruments from a request
+	try:
+		all_instruments = []
+		book_summaries = deribit.get_book_summary_by_currency(_currency, _kind)
+		for book in book_summaries:
+			all_instruments.append(book['result']['instrument_name'])
+		return all_instruments
+	except Exception as e:
+		print('Error in create_instruments: ', e)
 
 def extract_greeks(instrument, ob):
 	return [instrument, ob['greeks']]
@@ -50,6 +47,9 @@ def check_memory(_list, max_len = 1000):
 	if len(_list) > max_len:
 		_list = []
 	return _list
+
+
+all_option_instruments = create_instruments('BTC', 'option')
 
 pausetime = 5
 collected_trades = []
