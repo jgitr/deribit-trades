464c5fe699305be58a3bbec2f2b3eee4164b1790
Julian <julian.winkel@hu-berlin.de>
2022-10-10 18:25:31 +0200

    adjusted filenames for scraper

--- load_credentials.py
+++ load_credentials.py
@@ -4,8 +4,8 @@ import keyring
 keyring.set_keyring([keyring.backend.get_all_keyring()][0][0])
 
 try:
-    CLIENT_ID = keyring.get_password('lax', 'deribit-trades-clientid')
-    CLIENT_SECRET = keyring.get_password('lax', 'deribit-trades-pw')
+    CLIENT_ID = keyring.get_password('ubuntu', 'deribit-clientid')
+    CLIENT_SECRET = keyring.get_password('ubuntu', 'deribit-clientpw')
 except:
     CLIENT_ID = ''
     CLIENT_SECRET = ''
