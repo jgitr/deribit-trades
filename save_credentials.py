563c97bbb3440d6264167bd8c60847404a060d04
Julian <julian.winkel@hu-berlin.de>
2022-10-10 18:21:16 +0200

    Choose Keyring Backend automatically

--- save_credentials.py
+++ save_credentials.py
@@ -1,6 +1,9 @@
 import keyring
 
     
+# Choose Backend
+keyring.set_keyring([keyring.backend.get_all_keyring()][0][0])
+
 print('Enter a filename to store your CLIENTID')
 clientid_name = input()
 
@@ -23,7 +26,7 @@ keyring.set_password(username, password_name, password)
 retrieve_clientid = 'keyring.get_password({}, {})'.format(str(username), str(clientid_name))
 retrieve_password = 'keyring.get_password({}, {})'.format(str(username), str(password_name))
 
-print('Paste the following commands in the given order in main.py under CLIENT_ID and CLIENT_SECRET')
+print('Paste the following commands in the given order in load_credentials.py under CLIENT_ID and CLIENT_SECRET')
 print(retrieve_clientid)
 print(retrieve_password)
 
